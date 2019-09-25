be2c4eb4dc2444fbff9a69a8e9e01e02b897ff30
jtx007 <jamesjacobthomas7@gmail.com>
2019-09-25 14:32:40 -0400

    tested api request

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import popLockersAPI from '../api/poplockersAPI'
 export default class Index extends Component {
 
 async componentDidMount() {
-    const response = await popLockersAPI.get('/user/james')
+    const response = await popLockersAPI.get('user/james')
     console.log(response.data)
 }
     render() {
