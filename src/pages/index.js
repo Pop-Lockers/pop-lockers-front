70840d046240f1ef149b654402cf48cdaabf473b
Angelo Spampinato <spampinatodesign@gmail.com>
2019-09-26 13:16:25 -0400

    back end connected

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import React from "react"
-import { BrowserRouter } from 'react-router-dom'
 
 
 import Routes from "../components/Routes"
 
 export default () => {
     return (
-      <BrowserRouter>
         <div>
             <Routes />
         </div>
-      </BrowserRouter>  
     )
 }
