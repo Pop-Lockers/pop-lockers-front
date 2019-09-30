3b78bc790239803515f704f6c5a058efb817666d
Regina Mc Guire <rmcguire@antelecom.net>
2019-09-29 22:16:46 -0700

    Add fetch statement

--- src/pages/invoices.js
+++ src/pages/invoices.js
@@ -2,44 +2,35 @@ import React, { Component} from 'react'
 import {Table} from 'reactstrap'
 import Invoice from '../components/invoice'
 
-const invoices = [{
-    'invoice_id': 1,
-    'address': '130 Beech St, Long Beach, CA',
-    'phone_number': '310-555-1212',
-    'amount': 59,
-    'date': '09/23/19',
-    'make': 'Ford',
-    'model': 'F-150',
-    'year': 2010,
-    'color': 'white',
-    'license_plate_number': 'PXD234',
-    'drivers_license_number': 'N6532571',
-    'license_state': 'CA',
-    'plate_state': 'WA',
-    'user_id': 101
-},
-{
-    "invoice_id": 2,
-    "address": "132 cornelia st",
-    "phone_number": "(555)555-5555",
-    "amount": 50000000,
-    "make": "Tesla",
-    "model": "Model 3",
-    "year": 5000,
-    "color": "pink",
-    "license_plate_number": "CASIU31",
-    "drivers_license_number": "DA83838",
-    "license_state": "NY",
-    "plate_state": "NY", 
-    "user_id": 200
-}]
+
 
 export default class Invoices extends Component{
+  state = {
+    invoices: []
+  }
+    componentDidMount() {
+        try {
+          fetch('http://127.0.0.1:5000/invoices').then((response)=> {
+          response.json().then(data => {
+            if (data.error) {
+              console.log(data.error)
+            } else {
+              this.setState ({
+                invoices: data.invoices
+              })
+            }
+          })
+        })
+      } catch (e) {
+        console.log('Error loading data')
+      }
+    }
     render() {
     return (
         
         <div>
             <h1>Invoices</h1>
+          
             <Table responsive>
             <thead>
           <tr>
@@ -59,7 +50,7 @@ export default class Invoices extends Component{
           </tr>
         </thead>
         <tbody>
-            {invoices.map((invoice, index) => (<Invoice invoice={invoice} key={index} />))}
+            {this.state.invoices.map((invoice, index) => (<Invoice invoice={invoice} key={index} />))}
             
         </tbody>
             </Table>
