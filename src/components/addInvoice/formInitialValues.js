8ba10b429994240b77513b422947489c761467ca
Andrew Horn <arhorn@smcm.edu>
2019-10-01 00:31:38 -0500

    all initial values now blank

--- src/components/addInvoice/formInitialValues.js
+++ src/components/addInvoice/formInitialValues.js
@@ -1,14 +1,14 @@
 export default {
-  address: 'asdf',
-  phoneNumber: 'asdf',
-  amount: 'asdf',
-  date: 'asdf',
-  make: 'asdf',
-  model: 'asdf',
-  year: 'asdf',
-  color: 'asdf',
-  licensePlateNumber: 'adsf',
-  driversLicenseNumber: 'asdf',
-  driversLicenseState: 'asdf',
-  licensePlateState: 'asdf'
+  address: '',
+  phoneNumber: '',
+  amount: '',
+  date: '',
+  make: '',
+  model: '',
+  year: '',
+  color: '',
+  licensePlateNumber: '',
+  driversLicenseNumber: '',
+  driversLicenseState: '',
+  licensePlateState: ''
 };
