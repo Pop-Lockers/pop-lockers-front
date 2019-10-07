66450fb98226ba01e7c00df8dcca07c57e64779a
Angelo Spampinato <spampinatodesign@gmail.com>
2019-10-07 13:41:54 -0400

    about to switch branches to add-invoice

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,89 +1,91 @@
-import React, { Component, Fragment } from 'react'
-import { Redirect } from '@reach/router'
-import {Form, FormGroup, Button, Label, Input} from 'reactstrap'
-import Layout from '../components/Layout/Layout'
-import popLockersAPI from '../api/poplockersAPI'
-import loginStyles from '../styles/login.module.css'
-import { LoginContext } from '../components/loginContext/loginContext'
+import React, { Component, Fragment } from "react"
+import { Redirect } from "@reach/router"
+import { Form, FormGroup, Button, Label, Input } from "reactstrap"
+import Layout from "../components/Layout/Layout"
+import popLockersAPI from "../api/poplockersAPI"
+import loginStyles from "../styles/login.module.css"
+import { LoginContext } from "../components/loginContext/loginContext"
 class Login extends Component {
+  state = {
+    username: "",
+    password: "",
+  }
 
-    state = {
-        username: '',
-        password: ''
-    }
+  handleInputChange = event => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
 
-    handleInputChange = (event) => {
-        const { name, value } = event.target
-        this.setState({
-            [name]: value
-        })
-    }
-
-    handleFormSubmit = async (event) => {
-        event.preventDefault()
-        const response = await popLockersAPI.post("/auth",
-        {
-            "username": this.state.username, "password": this.state.password
+  handleFormSubmit = async event => {
+    event.preventDefault()
+    const response = await popLockersAPI.post(
+      "/auth",
+      {
+        username: this.state.username,
+        password: this.state.password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        console.log(response.data.access_token)
-        localStorage.setItem('user_id', response.data.user_id)
-        localStorage.setItem('token', response.data.access_token)
-        this.props.setUser(response.data.user_id)
-        this.props.setToken(response.data.access_token)
-        
-    }
+      }
+    )
+    console.log(response.data.access_token)
+    localStorage.setItem("user_id", response.data.user_id)
+    localStorage.setItem("token", response.data.access_token)
+    this.props.setUser(response.data.user_id)
+    this.props.setToken(response.data.access_token)
+  }
 
-    renderFormOrRedirect = () => {
-        const { token, user_id } = this.props
-        if (token && user_id) {
-            return (
-                
-                <Redirect from="/login" to="/add-invoice" noThrow/>
-            )
-        } else {
-            return (
-                    <Form className={loginStyles.form} onSubmit={this.handleFormSubmit}>
-                        <FormGroup>
-                            <Label for="enterUsername">Enter Username</Label>
-                            <Input onChange={this.handleInputChange} name="username" value={this.state.username} type="text"  />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="enterPassword">Enter Password</Label>
-                            <Input onChange={this.handleInputChange} name="password" value={this.state.password} type="password" />
-                        </FormGroup>
-                        <Button type="submit">Submit</Button>
-                    </Form>
-            )
-        }
-        
+  renderFormOrRedirect = () => {
+    const { token, user_id } = this.props
+    console.log(token, user_id)
+    if (token && user_id) {
+      return <Redirect from="/login" to="/add-invoice" noThrow />
+    } else {
+      return (
+        <Form className={loginStyles.form} onSubmit={this.handleFormSubmit}>
+          <FormGroup>
+            <Label for="enterUsername">Enter Username</Label>
+            <Input
+              onChange={this.handleInputChange}
+              name="username"
+              value={this.state.username}
+              type="text"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="enterPassword">Enter Password</Label>
+            <Input
+              onChange={this.handleInputChange}
+              name="password"
+              value={this.state.password}
+              type="password"
+            />
+          </FormGroup>
+          <Button type="submit">Submit</Button>
+        </Form>
+      )
     }
+  }
 
-    render() {
-        return (
-            <>
-                {this.renderFormOrRedirect()}
-            </>
-
-        )
-    }
+  render() {
+    return <>{this.renderFormOrRedirect()}</>
+  }
 }
 
 const LoginWithConsumer = () => {
-    return (
-        <Layout>
-            <LoginContext.Consumer>
-                {(value) => {
-                    return <Login {...value} />
-                }}
-                
-            </LoginContext.Consumer>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <LoginContext.Consumer>
+        {value => {
+          return <Login {...value} />
+        }}
+      </LoginContext.Consumer>
+    </Layout>
+  )
 }
 
 export default LoginWithConsumer
