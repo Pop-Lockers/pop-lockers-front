cab60daf5d1ed15d28b049d58524dd5f7bd96352
Andrew Horn <arhorn@smcm.edu>
2019-10-07 12:55:28 -0500

    corrected path for imported AddInvoice component

--- src/pages/add-invoice.js
+++ src/pages/add-invoice.js
@@ -1,5 +1,5 @@
 import React from "react"
-import AddInvoice from "../components/addInvoice/addInvoice"
+import AddInvoice from "../components/AddInvoice/AddInvoice"
 import Layout from "../components/Layout/Layout"
 import { LoginContext } from "../components/loginContext/loginContext"
 import { Redirect } from "@reach/router"
