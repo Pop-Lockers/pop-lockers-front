7df5baff1db3283614f02ae1f9e0ba75a4df9764
Regina Mc Guire <rmcguire@antelecom.net>
2019-10-09 06:46:21 -0700

    Change intial token and user_id state for Netlify

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,11 +4,12 @@ import Footer from '../Footer/Footer'
 import layoutStyles from './layout.module.css'
 import { LoginContext } from '../loginContext/loginContext'
 const Layout = ({ children }) => {
+  const [token, setToken] = useState('')
+  const [user_id, setUser] = useState('')
   if (typeof window !== `undefined`) {
-    const [token, setToken] = useState(localStorage.getItem('token') || '')
-    const [user_id, setUser] = useState(localStorage.getItem('user_id') || '')
+    token = setToken(localStorage.getItem('token'))
+    user_id = setUser(localStorage.getItem('user_id'))
   }
-
   return (
     <div className={layoutStyles.layout}>
       <LoginContext.Provider
