a2ae621ea26e65d78d1bd0b3d4260b178834880b
Andrew Horn <arhorn@smcm.edu>
2019-10-10 22:03:36 -0500

    Layout state now initializes with localStorage value to prevent infinite rerender

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import NavBar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-import layoutStyles from './layout.module.css'
-import { LoginContext } from '../loginContext/loginContext'
+import React, { useState } from "react"
+import NavBar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
+import layoutStyles from "./layout.module.css"
+import { LoginContext } from "../loginContext/loginContext"
 const Layout = ({ children }) => {
-  const [token, setToken] = useState('')
-  const [user_id, setUser] = useState('')
-  if (typeof window !== `undefined`) {
-    setToken(localStorage.getItem('token'))
-    setUser(localStorage.getItem('user_id'))
-  }
+  const [token, setToken] = useState(
+    (typeof window !== `undefined` && localStorage.getItem("token")) || ""
+  )
+  const [user_id, setUser] = useState(
+    (typeof window !== `undefined` && localStorage.getItem("user_id")) || ""
+  )
   return (
     <div className={layoutStyles.layout}>
       <LoginContext.Provider
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
           token,
           user_id,
           setToken,
-          setUser
+          setUser,
         }}
       >
         <NavBar />
